26714c61ca013d67c5f825c15f417c12da7c529d
AetherFabian <fabian_galarza@hotmail.com>
2022-07-26 12:06:10 -0600

    feat: add routes of the app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import './App.css';
+import MainPage from './views/main.js';
+import TrackMaps from './views/maps.js';
+import FeedBack from './views/feedback.js';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/map" element={<TrackMaps />} />
+        <Route path="/feedback" element={<FeedBack />} />
+      </Routes>
+    </Router>
   );
 }
 
