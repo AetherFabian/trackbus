6ed9e8bbe59b03a0181754bd3036a692db902747
AetherFabian <fabian_galarza@hotmail.com>
2022-08-07 22:02:04 -0600

    feat: dependencie select and feedback toute added.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import './App.css';
 import MainPage from './views/main.js';
 import TrackMaps from './views/maps.js';
-//import FeedBack from './views/feedback.js';
+import Feedback from './views/feedback.js';
 
 const App = () => {
   return (
@@ -10,6 +10,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/map" element={<TrackMaps />} />
+        <Route path="/feedback" element={<Feedback />} />
       </Routes>
     </Router>
   );
