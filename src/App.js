43779814c106f3d7b66279027b39f6e38b221e76
AetherFabian <fabian_galarza@hotmail.com>
2022-08-07 20:13:31 -0600

    feat: maps view showing and tracking in the google map by selected routes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import './App.css';
 import MainPage from './views/main.js';
 import TrackMaps from './views/maps.js';
-import FeedBack from './views/feedback.js';
+//import FeedBack from './views/feedback.js';
 
 const App = () => {
   return (
@@ -10,7 +10,6 @@ const App = () => {
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/map" element={<TrackMaps />} />
-        <Route path="/feedback" element={<FeedBack />} />
       </Routes>
     </Router>
   );
--- src/views/maps.js
+++ src/views/maps.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import './../css/webapp.css'
-import { Map, GoogleApiWrapper, Marker, InfoWindow } from "google-maps-react";
+import { Map, GoogleApiWrapper, Marker } from "google-maps-react";
 import Http from "../libs/trackBusApi";
 import { Link } from 'react-router-dom';
+import CustomPopup from '../components/portal';
 //import Routes from '../components/routes';
 
 class TrackMaps extends React.Component {
@@ -12,54 +13,42 @@ class TrackMaps extends React.Component {
         setSignalsState: [],
         selectedRoute: '',
         activateMarker: null,
-        time: '',
+        visibility: false,
+        buttonVisibility: false,
+    }
+
+    popupCloseHandler = (e) => {
+        this.setState({ visibility: e });
+    };
+
+
+    handleToggleOpen = (marker) => {
+        this.setState({ visibility: !this.state.visibility, activateMarker: marker });
     }
 
     componentDidMount = () => {
         this.getData();
-        //this.timer();
     };
 
     getData = async () => {
-        const response = await Http.get_routes();
+        const response = await Http.instance.get_routes();
         this.setState({ routes: response });
     };
 
     getSignals = async (name) => {
+        console.log(name);
+        console.log(this.state.selectedRoute);
+        if (name !== this.state.selectedRoute) {
+            clearTimeout(this.timerID);
+        }
         if (name !== '') {
-            const response = await Http.get_signals(name);
+            const response = await Http.instance.get_signals(name);
             this.setState({ setSignalsState: response, selectedRoute: name });
+            /*this.timerID = setTimeout(() => {
+                this.getSignals(name);
+            }, 20000);*/
         }
     }
-
-    /*handleActiveMarker = (marker) => {
-        if (marker === this.state.activateMarker) {
-            return;
-        }
-        this.setState({ activateMarker: marker });
-        return (<InfoWindow children={marker.cordinates_x} onCloseClick={() => this.setState({ activateMarker: null })}>
-            {marker.spoted_at}
-                </InfoWindow>);
-    };*/
-
-    timer = () => {
-        setTimeout(() => {
-            this.getSignals(this.state.selectedRoute);
-            this.timer();
-        }, 20000);
-    };
-
-    timeWatch = () => {
-        const date = new Date();
-        const hour = date.getHours();
-        const minute = date.getMinutes();
-        const second = date.getSeconds();
-        const time = `${hour}:${minute}:${second}`;
-        this.setState({time: time});
-        console.log(this.time);
-        this.timeWatch();
-    }
-
 
     mapStyles =
         [{
@@ -105,49 +94,46 @@ class TrackMaps extends React.Component {
                         style={{
                             width: '80%',
                             height: '89%',
-                            position: 'relative',
                         }
                         }
-                        onClick={() => this.setState({ activateMarker: null })}
                         initialCenter={{ lat: 28.6575657, lng: -106.084936 }}>
                         {this.state.setSignalsState.map((marker) => {
-                            return <Marker key={marker.spoted_at}
+                            return <Marker
+                                key={marker.spoted_at}
                                 position={{ lng: marker.cordinates_y, lat: marker.cordinates_x }}
-                                icon={{
-                                    url: process.env.PUBLIC_URL + "images/marker.png",
-                                    size: new this.props.google.maps.Size(35, 50),
-                                    scaledSize: new this.props.google.maps.Size(35, 50),
-                                }}
-                                label={{
-                                    text: this.timeWatch(marker),
-                                    color: '#fff',
-                                    fontSize: '12px',
-                                    fontWeight: '10',
-                                }}
-                                animation={this.props.google.maps.Animation.DROP}
-                                onClick={(e) => this.handleActiveMarker(marker, e)}
+                                onClick={() => this.handleToggleOpen(marker)}
                             >
                             </Marker>
                         })}
                     </Map>
                     <div className="menu-routes">
+
                         <div className="menu-routes-title">
+
                             <h5 style={{ color: '#fff' }}>Bus Stops</h5>
                         </div>
                         <div className="collection col s4 contenedor">
                             {this.state.routes.map((route) => {
-                                return <Link className="collection-item" key={route.name} to={'#!'} onClick={() => this.getSignals(route.name)}>
-                                    {route.name}
+                                return <Link className="collection-item" key={route.bus_id} to={'#!'} onClick={() => this.getSignals(route.bus_name)}>
+                                    {route.bus_name} 
                                 </Link>
                             })}
                         </div>
                     </div>
+                    {this.state.visibility === true ? <CustomPopup
+                        onClose={this.popupCloseHandler}
+                        show={this.state.visibility}
+                        showButton={this.state.buttonVisibility}
+                        title={this.state.selectedRoute}
+                        signals={this.state.setSignalsState}
+                        nameStop={this.state.activateMarker.name_stop}/> : null}
                 </div>
             </React.Fragment>
         );
     }
 
 }
+
 
 export default GoogleApiWrapper({
     apiKey: 'AIzaSyDQ0hak9HMuVbNpwzx_91uLYLuLRiCXvUA'
